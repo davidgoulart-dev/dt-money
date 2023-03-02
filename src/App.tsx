b88a37019f385796b655bacf08ea95952d4b844b
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-03-01 22:06:48 -0300

    estilos globais criados
    estilos globais implementados

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export function App() {
   return (
     <div className="App">
       <h1>Hello Word</h1>
+      <GlobalStyle />
       
     </div>
   );
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,10 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 :root {
     --background: #F0F2F5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+    --blue-light: #6933FF;
+    --text-title: #363F5F;
+
+    --text-body: #969CB3;
+    --shape: #FFFFFF;
 
 }
 body {
     background: var(--background);
+    -webkit-font-smoothing: antialiased;
 }
 
 * {
@@ -13,6 +21,14 @@ body {
     padding: 0;
     box-sizing: border-box;
 
+}
+html {
+    @media (max-width: 1080px) { 
+        font-size: 93.75%;
+    }
+    @media (max-width: 720px) { 
+        87.5%;
+    }
 }
 body, input, textarea, button {
     font-family: 'Heebo', sans-serif;
@@ -24,5 +40,13 @@ font-weight: 400;
 
 h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
+}
+button {
+    cursor: pointer;
+}
+
+[disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
 }
 `
