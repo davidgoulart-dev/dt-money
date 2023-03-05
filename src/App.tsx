e19d5c4e0359197c12387da26ec65a5f7e51dd8e
David Goulart <119085211+davidgoulart-dev@users.noreply.github.com>
2023-03-05 21:34:05 +0000

    Novo componente para o modal criado

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import Modal from 'react-modal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -21,12 +22,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard/>
       <GlobalStyle />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
-        onRequestClose={handleCloseNewTransactionModal}>
-          <h2>Cadastrar transação</h2>
-
-        </Modal>
+      <NewTransactionModal 
+      isOpen={ isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}/>
+     
       
     </>
   );
