f897c10d7c6696d21a27900f9bbdd6401bd47c45
David Goulart <119085211+davidgoulart-dev@users.noreply.github.com>
2023-03-05 22:35:10 +0000

    Estilização do modal completa
    Estilização do modal completa, em seguida vamos cadastra novas transações na api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from '../NewTransactionModal/styles';
+import closeImg  from '../../assets/close.svg';
 interface NewTransactionModalProps { 
     isOpen: boolean;
     onRequestClose: () => void;
@@ -7,8 +9,24 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     return (
         <Modal 
         isOpen={isOpen} 
-        onRequestClose={onRequestClose}>
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content">
+          <button 
+          type="button" 
+          onClick={onRequestClose} 
+          className="react-modal-close">
+            <img src={closeImg} alt="Fechar modal" />
+            
+            </button>  
+
+        <Container>
           <h2>Cadastrar transação</h2>
+          <input  placeholder="Título" />
+          <input type="number" placeholder="Valor" />
+          <input  placeholder="Categoria" />
+            <button type="submit">Cadastrar</button>
+        </Container>
 
         </Modal>
     )
