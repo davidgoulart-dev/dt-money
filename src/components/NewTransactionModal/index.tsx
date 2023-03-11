5753178caed76dceb0256fa0f324bf02f7f0e25e
David Goulart <119085211+davidgoulart-dev@users.noreply.github.com>
2023-03-10 22:16:29 -0300

    Banco de dados criado
    banco de dados implementado na aplicação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import closeImg  from '../../assets/close.svg';
 import incomeImg from '../../assets/Entradas.svg';
 import outcomeImg from '../../assets/Saídas.svg';
 import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 interface NewTransactionModalProps { 
     isOpen: boolean;
     onRequestClose: () => void;
@@ -17,12 +18,13 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-         console.log({
+         const data = {
               title,
               value,
               category,
               type,
-         })
+         }
+         api.post ('/transactions', data)
     }
    
     return (
